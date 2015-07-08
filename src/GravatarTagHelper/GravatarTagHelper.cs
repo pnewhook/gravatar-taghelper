96b7643c33f70507318eaa5ce5096c2fd6e9050f
NM <neutmute@neutmute.nm>
2015-07-08 12:37:43 +1000

    added default and size attributes https://en.gravatar.com/site/implement/images/

--- src/GravatarTagHelper/GravatarTagHelper.cs
+++ src/GravatarTagHelper/GravatarTagHelper.cs
@@ -7,26 +7,40 @@ namespace GravatarTagHelper
     [TargetElement("img", Attributes=EmailAttributeName)]
 	public class GravatarTagHelper : TagHelper
 	{
-        private const string EmailAttributeName="gravatar-email";
-        
+        private const string EmailAttributeName = "gravatar-email";
+
         [HtmlAttributeName(EmailAttributeName)]
         public string EmailAddress { get; set; }
-        
-		public override void Process(TagHelperContext context, TagHelperOutput output)
+        public int? GravatarSize { get; set; }
+
+        public string GravatarDefault { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            using(var md5 = MD5.Create())
-            {                
+            using (var md5 = MD5.Create())
+            {
                 byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(EmailAddress));
-                
-                // Build the final string by converting each byte
+
+                // Build the hash string by converting each byte
                 // into hex and appending it to a StringBuilder
                 StringBuilder sb = new StringBuilder();
-                for (int i=0;i<hash.Length;i++)
+                for (int i = 0; i < hash.Length; i++)
                 {
                     sb.Append(hash[i].ToString("x2"));
                 }
-                output.Attributes["src"] ="http://www.gravatar.com/avatar/" +  sb.ToString();
+
+                var url = "http://www.gravatar.com/avatar/" + sb.ToString();
+                if (!string.IsNullOrEmpty(GravatarDefault))
+                {
+                    url += "?d=" + GravatarDefault;
+                }
+
+                if (GravatarSize != null)
+                {
+                    url += "?s=" + GravatarSize;
+                }
+                output.Attributes["src"] = url;
             }
-	   }
+        }
     }
 }
